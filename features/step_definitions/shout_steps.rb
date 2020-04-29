3650b79b00e94f2065a7c0c5863178bdf8b2e9f8
Djeber Merah <djeber.merah@smartbear.com>
2020-04-29 14:36:45 +0200

    changes to test

--- features/step_definitions/shout_steps.rb
+++ features/step_definitions/shout_steps.rb
@@ -1,28 +1,30 @@
+# frozen_string_literal: true
+
 require 'shouty'
 require 'coordinate'
 
-ARBITRARY_MESSAGE = "Hello, world"
+ARBITRARY_MESSAGE = 'Hello, world'
 
 Before do
   @shouty = Shouty.new
 end
 
 Given('Lucy is at {int}, {int}') do |x, y|
-  @shouty.set_location("Lucy", Coordinate.new(x, y))
+  @shouty.set_location('Lucy', Coordinate.new(x, y))
 end
 
 Given('Sean is at {int}, {int}') do |x, y|
-  @shouty.set_location("Sean", Coordinate.new(x, y))
+  @shouty.set_location('Sean', Coordinate.new(x, y))
 end
 
 When('Sean shouts') do
-  @shouty.shout("Sean", ARBITRARY_MESSAGE)
+  @shouty.shout('Sean', ARBITRARY_MESSAGE)
 end
 
 Then('Lucy should hear Sean') do
-  expect(@shouty.shouts_heard_by("Lucy").size).to eq(1)
+  expect(0).to eq(1)
 end
 
 Then('Lucy should hear nothing') do
-  expect(@shouty.shouts_heard_by("Lucy").size).to eq(0)
+  expect(@shouty.shouts_heard_by('Lucy').size).to eq(0)
 end
